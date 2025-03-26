146fbe28cf7db7de1455eb8f5dfdbe9f249edd32
sidney <sidney@jigsaw.nl>
2025-03-26 20:54:22 +0100

    The example code can now reproduce the simple v1 QR code from the standard.

--- encoding_example.py
+++ encoding_example.py
@@ -1,4 +1,4 @@
-#! /usr/bin/env python3
+#! /usr/bin/env -S python3 -B
 
 """An example encoder for a version 1-M QR Code symbol.
 This example uses numeric mode.
@@ -6,6 +6,10 @@ This example uses numeric mode.
 
 from enum import IntEnum
 from PIL import Image, ImageDraw
+
+from binary_codes import format_information_code_remainder, version_information_code_remainder
+from reed_solomon_code import calculate_reed_solomon_polynomial, reed_solomon_code_remainder
+
 
 class ModuleValue(IntEnum):
     QUIET_ZONE_0          = 10
@@ -24,6 +28,25 @@ class ModuleValue(IntEnum):
     DATA_ERC_1            = 81
     INDETERMINATE         = 99
 
+
+class DataMaskingPattern(IntEnum):
+    Pattern0 = 0b000
+    Pattern1 = 0b001
+    Pattern2 = 0b010
+    Pattern3 = 0b011
+    Pattern4 = 0b100
+    Pattern5 = 0b101
+    Pattern6 = 0b110
+    Pattern7 = 0b111
+
+
+class ErrorCorrectionLevel(IntEnum):
+    L = 0b01  # Allows recovery of  7% of bad modules.
+    M = 0b00  # Allows recovery of 15% of bad modules.
+    Q = 0b11  # Allows recovery of 25% of bad modules.
+    H = 0b10  # Allows recovery of 30% of bad modules.
+
+
 render_colormap = {
     ModuleValue.QUIET_ZONE_0          : '#ffffff',
     ModuleValue.FINDER_PATTERN_0      : '#ffffff',
@@ -33,14 +56,15 @@ render_colormap = {
     ModuleValue.TIMING_PATTERN_1      : '#000000',
     ModuleValue.ALIGNMENT_PATTERN_0   : 'yellow',
     ModuleValue.ALIGNMENT_PATTERN_1   : 'orange',
-    ModuleValue.FORMAT_INFORMATION_0  : 'cyan',
-    ModuleValue.FORMAT_INFORMATION_1  : 'blue',
+    ModuleValue.FORMAT_INFORMATION_0  : '#ffffff',
+    ModuleValue.FORMAT_INFORMATION_1  : '#000000',
     ModuleValue.VERSION_INFORMATION_0 : '#00cc00',
     ModuleValue.VERSION_INFORMATION_1 : '#00ff00',
-    ModuleValue.DATA_ERC_0            : '#ffcccc',
-    ModuleValue.DATA_ERC_1            : '#ffcccc',
+    ModuleValue.DATA_ERC_0            : '#ffffff',
+    ModuleValue.DATA_ERC_1            : '#000000',
     ModuleValue.INDETERMINATE         : '#ff0000'
 }
+
 
 class QRCodeCanvas:
 
@@ -190,7 +214,20 @@ class QRCodeDrawer:
 
     def place_format_information_regions(self):
 
+        level = ErrorCorrectionLevel.M
+        pattern = DataMaskingPattern.Pattern2
+
+        format_data_bits = (level << 3) | pattern
+        print("@@@@", level, pattern, bin(format_data_bits))
+        format_ecc_bits = format_information_code_remainder(format_data_bits)
+
+        format_bits = (format_data_bits << 10) | format_ecc_bits
+
         # Don't forget to XOR the info with a fixed pattern!
+        format_bits ^= 0b101010000010010
+
+        #print("format bits: 0b{:015b}".format(format_bits))
+
         W = self.width
         H = self.height
 
@@ -199,18 +236,55 @@ class QRCodeDrawer:
             [(W-1, 8), (W-2, 8), (W-3, 8), (W-4, 8), (W-5, 8), (W-6, 8), (W-7, 8), (W-8, 8), (8, H-7), (8, H-6), (8, H-5), (8, H-4), (8, H-3), (8, H-2), (8, H-1)]
         ]
 
+        # Set the single module.
         self.set_module_value(8, H-8, ModuleValue.FORMAT_INFORMATION_1)
 
         for k in range(15):
+            fbit = ((format_bits >> k) & 1) != 0
             for copy in copies:
                 (x, y) = copy[k]
-                self.set_module_value(x, y, ModuleValue.FORMAT_INFORMATION_1 if k % 2 != 0 else ModuleValue.FORMAT_INFORMATION_0)
+                self.set_module_value(x, y, ModuleValue.FORMAT_INFORMATION_1 if fbit else ModuleValue.FORMAT_INFORMATION_0)
 
     def place_version_information_regions(self):
-        if self.version >=7:
+        if self.version >= 7:
+
+            version_bits = version_information_code_remainder(self.version) | (self.version << 12)
+
             for k in range(18):
-                self.set_module_value(k // 3, self.height - 11 + k % 3, ModuleValue.VERSION_INFORMATION_1 if k % 2 != 0 else ModuleValue.VERSION_INFORMATION_0)
-                self.set_module_value(self.width - 11 + k % 3, k // 3, ModuleValue.VERSION_INFORMATION_1 if k % 2 != 0 else ModuleValue.VERSION_INFORMATION_0)
+                vbit = (version_bits >> k) & 1 == 0
+                self.set_module_value(k // 3, self.height - 11 + k % 3, ModuleValue.VERSION_INFORMATION_1 if vbit else ModuleValue.VERSION_INFORMATION_0)
+                self.set_module_value(self.width - 11 + k % 3, k // 3, ModuleValue.VERSION_INFORMATION_1 if vbit else ModuleValue.VERSION_INFORMATION_0)
+
+    def get_indeterminate_positions(self):
+        positions = []
+        assert self.width % 2 == 1
+        for h_outer in range((self.width - 1) // 2):
+            # Note: the last three h_outer walks are to the left of the vertical timing pattern, the
+            # other ones are to the right of the vertical timing pattern.
+            for v in range(self.height):
+                y = (self.height - 1 - v) if h_outer % 2 == 0 else v
+                for h_inner in range(2):
+                    h = h_outer * 2 + h_inner
+                    x = (self.width - 1 - h)
+                    if x <= 6: # Skip vertical timing pattern.
+                        x -= 1
+                    #print("==>", h_outer, h_inner, "h", h, "v", v, "x", x, "y", y)
+                    value = self.get_module_value(x, y)
+                    if value == ModuleValue.INDETERMINATE:
+                        position = (x, y)
+                        positions.append(position)
+        return positions
+
+    def apply_data_masking_pattern(self, pattern, positions):
+        if pattern != DataMaskingPattern.Pattern2:
+            raise RuntimeError()
+        for (x, y) in positions:
+            value = self.get_module_value(x, y)
+            assert value in (ModuleValue.DATA_ERC_0, ModuleValue.DATA_ERC_1)
+            # Invert if the pattern condition is True
+            if x % 3 == 0:
+                value = ModuleValue.DATA_ERC_1 if value == ModuleValue.DATA_ERC_0 else ModuleValue.DATA_ERC_0
+                self.set_module_value(x, y, value)
 
     def render_as_image(self, filename: str, *, magnification: int = 1) -> None:
         self.canvas.render_as_image(filename, magnification = magnification)
@@ -223,6 +297,42 @@ class QRCodeDrawer:
                 if value == ModuleValue.INDETERMINATE:
                     count += 1
         return count
+
+
+class DataEncoder:
+    def __init__(self):
+        self.bits = []
+    def add_bits(self, s: str):
+        for c in s:
+            if c == '0':
+                self.bits.append(0)
+            if c == '1':
+                self.bits.append(1)
+
+    def add_error_correction_words(self, n: int):
+        # Convert data to 8-bit words.
+        assert len(self.bits) % 8 == 0
+        idx = 0
+        data = []
+        while idx != len(self.bits):
+            word = int("".join(map(str, self.bits[idx:idx+8])), 2)
+            data.append(word)
+            idx += 8
+
+        poly = calculate_reed_solomon_polynomial(n, strip=True)
+
+        erc = reed_solomon_code_remainder(data, poly)
+
+        for w in erc:
+            self.add_bits("{:08b}".format(w))
+
+        idx = 0
+        data = []
+        while idx != len(self.bits):
+            word = "".join(map(str, self.bits[idx:idx+8]))
+            print(word)
+            idx += 8
+
 
 def main():
 
@@ -283,7 +393,10 @@ def main():
         (40, 4)
     )
 
-    for version in range(1, 41):
+    magnification = 20
+
+    for version in (1, ):
+    #for version in range(1, 41):
         #print(f"Generating version {version} QR code...")
         qr = QRCodeDrawer(version, True)
         qr.place_quiet_zone()
@@ -293,9 +406,42 @@ def main():
         qr.place_alignment_patterns()
         qr.place_format_information_regions()
         qr.place_version_information_regions()
-        #qr.render_as_image(f"v{version}.png", magnification=magnification)
 
-        print(version, qr.count_indeterminate())
+        de = DataEncoder()
+
+        de.add_bits("0001")
+        de.add_bits("0000001000")
+
+        de.add_bits("0000001100")
+        de.add_bits("0101011001")
+        de.add_bits("1000011")
+
+        while len(de.bits) % 8 != 0:
+            de.add_bits("0")
+
+        padding = 0
+        while len(de.bits) // 8 != 16:
+            if padding == 0:
+                de.add_bits("11101100")
+            else:
+                de.add_bits("00010001")
+            padding = 1 - padding
+
+        de.add_error_correction_words(10)
+
+        positions = qr.get_indeterminate_positions()
+
+        assert len(positions) == len(de.bits)
+
+        for (p, b) in zip(positions, de.bits):
+            qr.set_module_value(*p, ModuleValue.DATA_ERC_1 if b else ModuleValue.DATA_ERC_0)
+
+        qr.apply_data_masking_pattern(DataMaskingPattern.Pattern2, positions)
+
+
+        qr.render_as_image(f"v{version}.png", magnification=magnification)
+
+
 
 if __name__ == "__main__":
     main()
