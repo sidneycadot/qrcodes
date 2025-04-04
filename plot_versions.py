a07abb6c50a4fc475d5abc427c8b8eb744ea8c1d
sidney <sidney@jigsaw.nl>
2025-04-04 11:02:30 +0200

    Plot version capacities.

---
+++ plot_versions.py
@@ -0,0 +1,38 @@
+#! /usr/bin/env -S python3 -B
+
+import matplotlib.pyplot as plt
+
+from enum_types import ErrorCorrectionLevel
+from lookup_tables import version_specifications
+
+data = {}
+for version_specification in version_specifications.values():
+    if version_specification.error_correction_level not in data:
+        data[version_specification.error_correction_level] = []
+    number_of_data_codewords = version_specification.total_number_of_codewords - version_specification.number_of_error_correcting_codewords
+    data[version_specification.error_correction_level].append((version_specification.version, number_of_data_codewords))
+
+plotspecs = [
+    (311, None),
+    (312, 10),
+    (313, 4)
+]
+
+for (subplotspec, xlim) in plotspecs:
+
+    plt.subplot(subplotspec)
+
+    for error_correction_level in ErrorCorrectionLevel:
+        xy = data[error_correction_level]
+        if xlim is not None:
+            xy = [(x, y) for (x, y) in xy if x <= xlim]
+        x, y = zip(*xy)
+        plt.plot(x, y, "-*", label=error_correction_level.name)
+
+    plt.xlabel("version")
+    plt.ylabel("data capacity [bytes]")
+    plt.grid()
+    plt.yscale('log')
+    plt.legend()
+
+plt.show()
