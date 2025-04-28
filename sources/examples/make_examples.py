b619a29ca7ec9f61437dbb550386519906ae6131
Sidney Cadot <sidney@jigsaw.nl>
2025-04-28 21:18:50 +0200

    Added ECI examples.

--- sources/examples/make_examples.py
+++ sources/examples/make_examples.py
@@ -1,12 +1,14 @@
 """Generate all examples."""
 
 import make_iso18004_examples
+import make_eci_examples
 import make_application_examples
 import make_miscellaneous_examples
 
 
 def main():
     make_iso18004_examples.main()
+    make_eci_examples.main()
     make_application_examples.main()
     make_miscellaneous_examples.main()
 
