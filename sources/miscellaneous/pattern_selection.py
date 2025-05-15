791dc6bcd500e1ad876127bc053f0ac118e50039
Sidney Cadot <sidney@jigsaw.nl>
2025-05-15 15:25:46 +0200

    Generating LaTeX.

--- sources/miscellaneous/pattern_selection.py
+++ sources/miscellaneous/pattern_selection.py
@@ -1,99 +1,150 @@
 #! /usr/bin/env -S python3 -B
 
-"""Write example QR codes."""
+"""Make overview of the pattern selections done in the standard."""
 
-from colorama import Fore, Style
+from enum import Enum
+from typing import NamedTuple
 
 from qrcode_generator.data_encoder import DataEncoder
 from qrcode_generator.enum_types import ErrorCorrectionLevel, EncodingVariant, DataMaskingPattern
-from qrcode_generator.qr_code import QRCodeDrawer
+from qrcode_generator.qr_code import make_qr_code
+from qrcode_generator.render.utilities import save_qrcode_as_png_file
 
 
-def run_testcase(testcase_name: str, de: DataEncoder, version: int, level: ErrorCorrectionLevel, reference_pattern: DataMaskingPattern):
-    filename = f"{testcase_name}.png"
-    qr = QRCodeDrawer(version, include_quiet_zone = False)
-    score1_map = {}
-    score2_map = {}
-    for pattern in DataMaskingPattern:
-        qr.place_data_and_error_correction_bits(de, level)
-        qr.apply_data_masking_pattern(pattern)
-        qr.place_version_information_placeholders()
-        qr.place_format_information_placeholders()
-        score1 = qr.score()
-        qr.place_version_information_patterns()
-        qr.place_format_information_patterns(level, pattern)
-        score2 = qr.score()
-        score1_map[pattern] = score1
-        score2_map[pattern] = score2
-        #if pattern == reference_pattern:
-        #    save_qrcode_as_png_file(filename, qr.canvas)
+class StandardEdition(Enum):
+    E2000 = 2000
+    E2006 = 2006
+    E2015 = 2015
+    E2024 = 2024
 
-    score1_best = min(score1_map.values())
-    score2_best = min(score2_map.values())
 
-    scores = []
-    for pattern in DataMaskingPattern:
-        pattern_str = f"P{pattern.name[-1]}"
-        score1 = score1_map[pattern]
-        score2 = score2_map[pattern]
-        score1_str = f"{score1}"
-        score2_str = f"{score2}"
-        spaces = 10 - len(score1_str) - len(score2_str)
-        if pattern == reference_pattern:
-            pattern_str = Fore.BLUE + pattern_str + Style.RESET_ALL
-        else:
-            pattern_str = Fore.YELLOW + pattern_str + Style.RESET_ALL
-        if score1 == score1_best:
-            score1_str = Fore.GREEN + score1_str + Style.RESET_ALL
-        else:
-            score1_str = Fore.RED + score1_str + Style.RESET_ALL
-        if score2 == score2_best:
-            score2_str = Fore.GREEN + score2_str + Style.RESET_ALL
-        else:
-            score2_str = Fore.RED + score2_str + Style.RESET_ALL
-        score_str = f"{score1_str},{score2_str}" + " " * spaces
-        scores.append(f"{pattern_str} {score_str}")
-    scores_str = "".join(scores).strip()
-    print(f"{testcase_name:20s}    {scores_str}")
+class Example(Enum):
+    Introduction = 1
+    StructuredAppendModeCombined = 2
+    StructuredAppendModePart1 = 3
+    StructuredAppendModePart2 = 4
+    StructuredAppendModePart3 = 5
+    StructuredAppendModePart4 = 6
+    Annex = 7
+
+
+class ExampleCodeSpec(NamedTuple):
+    edition: StandardEdition
+    location: str
+    pattern: DataMaskingPattern
+
+
+StructuredAppendExampleParity = 1
+
+class ExampleSpec(NamedTuple):
+    shortname: str
+    description: str
+    version: int
+    level: ErrorCorrectionLevel
+    data_encoder: DataEncoder
+
+example_specs = {
+    Example.Introduction: ExampleSpec(
+        shortname="introduction_example",
+        description="Introduction example",
+        version=1,
+        level=ErrorCorrectionLevel.M,
+        data_encoder=DataEncoder(EncodingVariant.SMALL).append_byte_mode_block(b"QR Code Symbol")
+    ),
+    Example.StructuredAppendModeCombined: ExampleSpec(
+        shortname="structured_append_mode_example_combined",
+        description="Structured Append Mode example, combined",
+        version=4,
+        level=ErrorCorrectionLevel.M,
+        data_encoder=DataEncoder(EncodingVariant.SMALL).append_alphanumeric_mode_block("ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ")
+    ),
+    Example.StructuredAppendModePart1: ExampleSpec(
+        shortname="structured_append_mode_example_1_of_4",
+        description="Structured Append Mode example, 1 of 4",
+        version=1,
+        level=ErrorCorrectionLevel.M,
+        data_encoder=DataEncoder(EncodingVariant.SMALL).append_structured_append_marker(0, 4, StructuredAppendExampleParity).append_alphanumeric_mode_block("ABCDEFGHIJKLMN")
+    ),
+    Example.StructuredAppendModePart2: ExampleSpec(
+        shortname="structured_append_mode_example_2_of_4",
+        description="Structured Append Mode example, 2 of 4",
+        version=1,
+        level=ErrorCorrectionLevel.M,
+        data_encoder=DataEncoder(EncodingVariant.SMALL).append_structured_append_marker(1, 4, StructuredAppendExampleParity).append_alphanumeric_mode_block("OPQRSTUVWXYZ0123")
+    ),
+    Example.StructuredAppendModePart3: ExampleSpec(
+        shortname="structured_append_mode_example_3_of_4",
+        description="Structured Append Mode example, 3 of 4",
+        version=1,
+        level=ErrorCorrectionLevel.M,
+        data_encoder=DataEncoder(EncodingVariant.SMALL).append_structured_append_marker(2, 4, StructuredAppendExampleParity).append_alphanumeric_mode_block("456789ABCDEFGHIJ")
+    ),
+    Example.StructuredAppendModePart4: ExampleSpec(
+        shortname="structured_append_mode_example_4_of_4",
+        description="Structured Append Mode example, 4 of 4",
+        version=1,
+        level=ErrorCorrectionLevel.M,
+        data_encoder=DataEncoder(EncodingVariant.SMALL).append_structured_append_marker(3, 4, StructuredAppendExampleParity).append_alphanumeric_mode_block("KLMNOPQRSTUVWXYZ")
+    ),
+    Example.Annex: ExampleSpec(
+        shortname="annex_example",
+        description="Annex example",
+        version=1,
+        level=ErrorCorrectionLevel.M,
+        data_encoder=DataEncoder(EncodingVariant.SMALL).append_numeric_mode_block("01234567")
+    )
+}
+
+
 
 
 def main():
 
-    # Note: the 2006 and 2015 editions of the standard uses PATTERN5 here, while the 2024 version
-    # of the standard uses PATTERN6.
-    de = DataEncoder(EncodingVariant.SMALL)
-    de.append_byte_mode_block(b"QR Code Symbol")
-    run_testcase("fig_1_page_7", de, 1, ErrorCorrectionLevel.M, DataMaskingPattern.PATTERN6)
+    with open("latex_tables.tex", "w") as fo:
 
-    de = DataEncoder(EncodingVariant.SMALL)
-    de.append_alphanumeric_mode_block("ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ")
-    run_testcase("fig_29-1_page_60", de, 4, ErrorCorrectionLevel.M, DataMaskingPattern.PATTERN4)
+        # |  Data Mask Pattern  |  Score term 1  |  Score term 2  |  Score term 3  |  Score term 4  |  Total score  |
+        # |---------------------+----------------+----------------+----------------+---------------+----------------|
+        # | Pattern 0           |                |        a                b              c              d          |
+        #
+        # Data Masking Scores for the 'xxxx example', lower is better. Pattern YYY has the lowest score, so should be selected.
 
-    de = DataEncoder(EncodingVariant.SMALL)
-    de.append_structured_append_marker(0, 4, 1)
-    de.append_alphanumeric_mode_block("ABCDEFGHIJKLMN")
-    run_testcase("fig_29-2_page_60", de, 1, ErrorCorrectionLevel.M, DataMaskingPattern.PATTERN0)
+        for (example, example_spec) in example_specs.items():
 
-    de = DataEncoder(EncodingVariant.SMALL)
-    de.append_structured_append_marker(1, 4, 1)
-    de.append_alphanumeric_mode_block("OPQRSTUVWXYZ0123")
-    run_testcase("fig_29-3_page_60", de, 1, ErrorCorrectionLevel.M, DataMaskingPattern.PATTERN7)
+            print("\\begin{table}", file=fo)
+            print("\\begin{tabular}[|c|c|c|c|c|c|c|]", file=fo)
+            print("\\hline", file=fo)
+            print("Symbol & Data Mask Pattern & Score term 1 & Score term 2 & Score term 3 & Score term 4 & Total score \\\\", file=fo)
+            print("\\hline", file=fo)
+            canvas = make_qr_code(
+                de=example_spec.data_encoder,
+                version=example_spec.version,
+                level=example_spec.level,
+                include_quiet_zone=False,
+                pattern=None
+            )
 
-    de = DataEncoder(EncodingVariant.SMALL)
-    de.append_structured_append_marker(2, 4, 1)
-    de.append_alphanumeric_mode_block("456789ABCDEFGHIJ")
-    run_testcase("fig_29-4_page_60", de, 1, ErrorCorrectionLevel.M, DataMaskingPattern.PATTERN7)
+            best_score = min(sum(value) for value in canvas.split_score_dict.values())
+            best_patterns = [p for p in DataMaskingPattern if sum(canvas.split_score_dict[p]) == best_score]
+            assert len(best_patterns) == 1
+            best_pattern = best_patterns[0]
 
-    de = DataEncoder(EncodingVariant.SMALL)
-    de.append_structured_append_marker(3, 4, 1)
-    de.append_alphanumeric_mode_block("KLMNOPQRSTUVWXYZ")
-    run_testcase("fig_29-5_page_60", de, 1, ErrorCorrectionLevel.M, DataMaskingPattern.PATTERN3)
+            for pattern in DataMaskingPattern:
 
-    # Note: the 2000 version of the standard uses PATTERN3 here, while the 2015 version
-    # of the standard uses PATTERN2.
-    de = DataEncoder(EncodingVariant.SMALL)
-    de.append_numeric_mode_block("01234567")
-    run_testcase("fig_i2_page_96", de, 1, ErrorCorrectionLevel.M, DataMaskingPattern.PATTERN2)
+                descriptor = save_qrcode_as_png_file(
+                    png_filename=f"{example_spec.shortname}_{{VERSION}}{{LEVEL}}p{{PATTERN}}.png",
+                    canvas=canvas,
+                    colormap='default',
+                    optimize_png=True
+                )
+
+                print(f"\\includegraphics{descriptor.png_filename} & Pattern~{pattern.name[-1]} & {canvas.split_score_dict[pattern][0]:6d} & {canvas.split_score_dict[pattern][1]:6d} & {canvas.split_score_dict[pattern][2]:6d} & {canvas.split_score_dict[pattern][3]:6d} & {sum(canvas.split_score_dict[pattern]):6d} \\\\", file=fo)
+
+            print("\\hline", file=fo)
+            print("\\end{tabular}", file=fo)
+            print(f"\\caption{{Score of {example_spec.description}. The best pattern is Pattern~{best_pattern.name[-1]} with a score of {best_score}.", file=fo)
+            print(f"\\label{{tab:score-{example_spec.shortname}}}", file=fo)
+            print("\\end{table}", file=fo)
+            print(file=fo)
 
 
 if __name__ == "__main__":
