7bb15a41353e997951ba641a832cbe6ae50f4b96
Sidney Cadot <sidney@jigsaw.nl>
2025-05-31 13:29:52 +0200

    Added relation between p, c, k, and r for each code.

--- sources/tests/test_version_specifications.py
+++ sources/tests/test_version_specifications.py
@@ -25,6 +25,9 @@ class TestVersionSpecificationsTable(unittest.TestCase):
 
             self.assertEqual(len(set(r for (count, (c, k, r)) in vspec.block_specification)), 1)
 
+            for (count, (c, k, r)) in vspec.block_specification:
+                self.assertEqual(c - k, 2 * r + vspec.p)
+
 
 if __name__ == "__main__":
     unittest.main()
